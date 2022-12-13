b57ac0b53228c48181a753a97f55c3586d568810
Yunosuke Sakai <qmkns246@gmail.com>
2022-12-13 13:26:42 +0900

    Initial Commit

---
+++ tca-alert/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  tca-alert
+//
+//  Created by sakai.yunosuke on 2022/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tca-alert/tca_alertApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_alertApp.swift
+//  tca-alert
+//
+//  Created by sakai.yunosuke on 2022/12/13.
+//
+
+import SwiftUI
+
+@main
+struct tca_alertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
